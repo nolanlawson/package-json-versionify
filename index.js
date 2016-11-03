8d81e5d61535566a80ae3bd660620b76d39c1ac9
Kirill Fomichev <fanatid@ya.ru>
2016-11-03 13:29:08 +0300

    Use browserify-package-json

--- index.js
+++ index.js
@@ -1,34 +1,6 @@
-"use strict";
-var through = require('through2');
+'use strict'
+const browserifyPackageJSON = require('browserify-package-json')
 
-/*
- * Convenience method for turning a through2 stream
- * into a full string. Otherwise we may just get chunks,
- * because Node buffers are chunked based on file size.
- */
-function streamToString(callback) {
-  var contents = '';
-
-  return through(
-    function (chunk, enc, next) {
-      contents += chunk.toString('utf8');
-      next();
-    },
-    function (next) { // flush function
-      callback.bind(this)(contents, next);
-    }
-  );
+module.exports = function (filename, options) {
+  return browserifyPackageJSON(filename, Object.assign(options, { only: 'version' }))
 }
-
-function versionify(filename) {
-  if (!/\b(?:package.json)$/.test(filename)) {
-    return through();
-  }
-  return streamToString(function (contents, next) {
-    var pkg = JSON.parse(contents);
-    this.push(JSON.stringify({version: pkg.version}));
-    next();
-  });
-}
-
-module.exports = versionify;
